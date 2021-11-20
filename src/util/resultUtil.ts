64325eaba6604a89fe8bc055cb07decd9663abac
autopp <autopp.inc@gmail.com>
2021-11-21 00:37:50 +0900

    feat: add result utility

--- src/util/resultUtil.ts
+++ src/util/resultUtil.ts
@@ -53,3 +53,88 @@ export function both5(t3: { coin: number }, t4: { coin: number }): boolean {
 export function trashingEstate(t3: { trashingEstate: boolean }, t4: { trashingEstate: boolean }): boolean {
   return any(t3, t4, 'trashingEstate')
 }
+
+export function resultOfAny<Topic extends string>(
+  t3: Result<Topic>,
+  t4: Result<Topic>,
+  k: Topic
+): { [t in Topic]: boolean } {
+  return { [k]: any(t3, t4, k) } as { [t in Topic]: boolean }
+}
+
+export function resultOfAll<Topic extends string>(
+  t3: Result<Topic>,
+  t4: Result<Topic>,
+  k: Topic
+): { [t in Topic]: boolean } {
+  return { [k]: all(t3, t4, k) } as { [t in Topic]: boolean }
+}
+
+export function resultOfAtLeastOnce<Coin extends number>(
+  t3: { coin: number },
+  t4: { coin: number },
+  coin: Coin
+): { [t in `atLeastOnce${Coin}`]: boolean } {
+  return { [`atLeastOnce${coin}`]: atLeastOnce(t3, t4, coin) } as { [t in `atLeastOnce${Coin}`]: boolean }
+}
+
+export function resultOfAtLeastOnce5(t3: { coin: number }, t4: { coin: number }): { atLeastOnce5: boolean } {
+  return resultOfAtLeastOnce(t3, t4, 5)
+}
+
+export function resultOfAtLeastOnce6(t3: { coin: number }, t4: { coin: number }): { atLeastOnce6: boolean } {
+  return resultOfAtLeastOnce(t3, t4, 6)
+}
+
+export function resultOfAtLeastOnce7(t3: { coin: number }, t4: { coin: number }): { atLeastOnce7: boolean } {
+  return resultOfAtLeastOnce(t3, t4, 7)
+}
+
+export function resultOfBoth<Coin extends number>(
+  t3: { coin: number },
+  t4: { coin: number },
+  coin: Coin
+): { [t in `both${Coin}`]: boolean } {
+  return { [`both${coin}`]: both(t3, t4, coin) } as { [t in `both${Coin}`]: boolean }
+}
+
+export function resultOfBoth5(t3: { coin: number }, t4: { coin: number }): { both5: boolean } {
+  return resultOfBoth(t3, t4, 5)
+}
+
+export function resultOfBothAndAtLeastOnce<B extends number, L extends number>(
+  t3: { coin: number },
+  t4: { coin: number },
+  bothCoin: B,
+  atLeastOnceCoin: L
+): { [t in `both${B}AndAtLeastOnce${L}`]: boolean } {
+  const key = `both${bothCoin}AndAtLeastOnce${atLeastOnceCoin}`
+  return {
+    [key]: both(t3, t4, bothCoin) && atLeastOnce(t3, t4, atLeastOnceCoin),
+  } as { [t in `both${B}AndAtLeastOnce${L}`]: boolean }
+}
+
+export function resultOfTrashingEstate(
+  t3: { trashingEstate: boolean },
+  t4: { trashingEstate: boolean }
+): { trashingEstate: boolean } {
+  return resultOfAny(t3, t4, 'trashingEstate')
+}
+
+export function resultOfTrashingEstateAndAtLeastOnce<Coin extends number>(
+  t3: { trashingEstate: boolean; coin: number },
+  t4: { trashingEstate: boolean; coin: number },
+  coin: Coin
+): { [t in `trashingEstateAndAtLeastOnce${Coin}`]: boolean } {
+  const key = `trashingEstateAndAtLeastOnce${coin}`
+  return { [key]: trashingEstate(t3, t4) && atLeastOnce(t3, t4, coin) } as {
+    [t in `trashingEstateAndAtLeastOnce${Coin}`]: boolean
+  }
+}
+
+export function resultOfTrashingEstateAndAtLeastOnce5(
+  t3: { trashingEstate: boolean; coin: number },
+  t4: { trashingEstate: boolean; coin: number }
+): { trashingEstateAndAtLeastOnce5: boolean } {
+  return resultOfTrashingEstateAndAtLeastOnce(t3, t4, 5)
+}
--- test/util/resultUtil.test.ts
+++ test/util/resultUtil.test.ts
@@ -7,6 +7,18 @@ import {
   atLeastOnce7,
   both,
   both5,
+  resultOfAll,
+  resultOfAny,
+  resultOfAtLeastOnce,
+  resultOfAtLeastOnce5,
+  resultOfAtLeastOnce6,
+  resultOfAtLeastOnce7,
+  resultOfBoth,
+  resultOfBoth5,
+  resultOfBothAndAtLeastOnce,
+  resultOfTrashingEstate,
+  resultOfTrashingEstateAndAtLeastOnce,
+  resultOfTrashingEstateAndAtLeastOnce5,
   trashingEstate,
 } from '@/util/resultUtil'
 
@@ -65,7 +77,7 @@ describe('atLeastOnce6', () => {
   })
 })
 
-describe('atLeastOnce6', () => {
+describe('atLeastOnce7', () => {
   it.each([
     { cond: 'turn3 reaches threshold', t3: { coin: 7 }, t4: { coin: 6 }, expected: true },
     { cond: 'turn4 reaches threshold', t3: { coin: 6 }, t4: { coin: 7 }, expected: true },
@@ -106,5 +118,212 @@ describe('trashingEstate', () => {
     { cond: 'both are false', t3: { trashingEstate: false }, t4: { trashingEstate: false }, expected: false },
   ])('when $cond, returns $expected', ({ t3, t4, expected }) => {
     expect(trashingEstate(t3, t4)).toEqual(expected)
+  })
+})
+
+describe('resultOfAny', () => {
+  it.each([
+    { cond: 'turn3 is true', t3: { key: true }, t4: { key: false }, expected: { key: true } },
+    { cond: 'turn4 is true', t3: { key: false }, t4: { key: true }, expected: { key: true } },
+    { cond: 'both are true', t3: { key: true }, t4: { key: true }, expected: { key: true } },
+    { cond: 'both are false', t3: { key: false }, t4: { key: false }, expected: { key: false } },
+  ])('when $cond, returns $expected', ({ t3, t4, expected }) => {
+    expect(resultOfAny(t3, t4, 'key')).toEqual(expected)
+  })
+})
+
+describe('resultOfAll', () => {
+  it.each([
+    { cond: 'turn3 is true', t3: { key: true }, t4: { key: false }, expected: { key: false } },
+    { cond: 'turn4 is true', t3: { key: false }, t4: { key: true }, expected: { key: false } },
+    { cond: 'both are true', t3: { key: true }, t4: { key: true }, expected: { key: true } },
+    { cond: 'both are false', t3: { key: false }, t4: { key: false }, expected: { key: false } },
+  ])('when $cond, returns $expected', ({ t3, t4, expected }) => {
+    expect(resultOfAll(t3, t4, 'key')).toEqual(expected)
+  })
+})
+
+describe('resultOfAtLeastOnce', () => {
+  it.each([
+    { cond: 'turn3 reaches threshold', t3: { coin: 5 }, t4: { coin: 4 }, expected: { atLeastOnce5: true } },
+    { cond: 'turn4 reaches threshold', t3: { coin: 4 }, t4: { coin: 5 }, expected: { atLeastOnce5: true } },
+    { cond: 'both reach threshold', t3: { coin: 5 }, t4: { coin: 5 }, expected: { atLeastOnce5: true } },
+    { cond: 'both do not reache threshold', t3: { coin: 4 }, t4: { coin: 4 }, expected: { atLeastOnce5: false } },
+  ])('when $cond, returns $expected', ({ t3, t4, expected }) => {
+    expect(resultOfAtLeastOnce(t3, t4, 5)).toEqual(expected)
+  })
+})
+
+describe('resultOfAtLeastOnce5', () => {
+  it.each([
+    { cond: 'turn3 reaches threshold', t3: { coin: 5 }, t4: { coin: 4 }, expected: { atLeastOnce5: true } },
+    { cond: 'turn4 reaches threshold', t3: { coin: 4 }, t4: { coin: 5 }, expected: { atLeastOnce5: true } },
+    { cond: 'both reach threshold', t3: { coin: 5 }, t4: { coin: 5 }, expected: { atLeastOnce5: true } },
+    { cond: 'both do not reache threshold', t3: { coin: 4 }, t4: { coin: 4 }, expected: { atLeastOnce5: false } },
+  ])('when $cond, returns $expected', ({ t3, t4, expected }) => {
+    expect(resultOfAtLeastOnce5(t3, t4)).toEqual(expected)
+  })
+})
+
+describe('resultOfAtLeastOnce6', () => {
+  it.each([
+    { cond: 'turn3 reaches threshold', t3: { coin: 6 }, t4: { coin: 5 }, expected: { atLeastOnce6: true } },
+    { cond: 'turn4 reaches threshold', t3: { coin: 5 }, t4: { coin: 6 }, expected: { atLeastOnce6: true } },
+    { cond: 'both reach threshold', t3: { coin: 6 }, t4: { coin: 6 }, expected: { atLeastOnce6: true } },
+    { cond: 'both do not reache threshold', t3: { coin: 5 }, t4: { coin: 5 }, expected: { atLeastOnce6: false } },
+  ])('when $cond, returns $expected', ({ t3, t4, expected }) => {
+    expect(resultOfAtLeastOnce6(t3, t4)).toEqual(expected)
+  })
+})
+
+describe('resultOfAtLeastOnce7', () => {
+  it.each([
+    { cond: 'turn3 reaches threshold', t3: { coin: 7 }, t4: { coin: 6 }, expected: { atLeastOnce7: true } },
+    { cond: 'turn4 reaches threshold', t3: { coin: 6 }, t4: { coin: 7 }, expected: { atLeastOnce7: true } },
+    { cond: 'both reach threshold', t3: { coin: 7 }, t4: { coin: 7 }, expected: { atLeastOnce7: true } },
+    { cond: 'both do not reache threshold', t3: { coin: 6 }, t4: { coin: 6 }, expected: { atLeastOnce7: false } },
+  ])('when $cond, returns $expected', ({ t3, t4, expected }) => {
+    expect(resultOfAtLeastOnce7(t3, t4)).toEqual(expected)
+  })
+})
+
+describe('resultOfBoth', () => {
+  it.each([
+    { cond: 'turn3 reaches threshold', t3: { coin: 5 }, t4: { coin: 4 }, expected: { both5: false } },
+    { cond: 'turn4 reaches threshold', t3: { coin: 4 }, t4: { coin: 5 }, expected: { both5: false } },
+    { cond: 'both reach threshold', t3: { coin: 5 }, t4: { coin: 5 }, expected: { both5: true } },
+    { cond: 'both do not reache threshold', t3: { coin: 4 }, t4: { coin: 4 }, expected: { both5: false } },
+  ])('when $cond, returns $expected', ({ t3, t4, expected }) => {
+    expect(resultOfBoth(t3, t4, 5)).toEqual(expected)
+  })
+})
+
+describe('resultOfBoth5', () => {
+  it.each([
+    { cond: 'turn3 reaches threshold', t3: { coin: 5 }, t4: { coin: 4 }, expected: { both5: false } },
+    { cond: 'turn4 reaches threshold', t3: { coin: 4 }, t4: { coin: 5 }, expected: { both5: false } },
+    { cond: 'both reach threshold', t3: { coin: 5 }, t4: { coin: 5 }, expected: { both5: true } },
+    { cond: 'both do not reache threshold', t3: { coin: 4 }, t4: { coin: 4 }, expected: { both5: false } },
+  ])('when $cond, returns $expected', ({ t3, t4, expected }) => {
+    expect(resultOfBoth5(t3, t4)).toEqual(expected)
+  })
+})
+
+describe('resultOfBothAndAtLeastOnce', () => {
+  it.each([
+    {
+      cond: 'reaches both coin threshold only',
+      t3: { coin: 5 },
+      t4: { coin: 5 },
+      expected: { both5AndAtLeastOnce6: false },
+    },
+    {
+      cond: 'reaches at least once coin threshold only',
+      t3: { coin: 6 },
+      t4: { coin: 4 },
+      expected: { both5AndAtLeastOnce6: false },
+    },
+    { cond: 'reaches both thresholds', t3: { coin: 5 }, t4: { coin: 6 }, expected: { both5AndAtLeastOnce6: true } },
+    {
+      cond: 'both do not reache threshold',
+      t3: { coin: 5 },
+      t4: { coin: 4 },
+      expected: { both5AndAtLeastOnce6: false },
+    },
+  ])('when $cond returns $expected', ({ t3, t4, expected }) => {
+    expect(resultOfBothAndAtLeastOnce(t3, t4, 5, 6)).toEqual(expected)
+  })
+})
+
+describe('resultOfTrashingEstate', () => {
+  it.each([
+    {
+      cond: 'turn3 is true',
+      t3: { trashingEstate: true },
+      t4: { trashingEstate: false },
+      expected: { trashingEstate: true },
+    },
+    {
+      cond: 'turn4 is true',
+      t3: { trashingEstate: false },
+      t4: { trashingEstate: true },
+      expected: { trashingEstate: true },
+    },
+    {
+      cond: 'both are true',
+      t3: { trashingEstate: true },
+      t4: { trashingEstate: true },
+      expected: { trashingEstate: true },
+    },
+    {
+      cond: 'both are false',
+      t3: { trashingEstate: false },
+      t4: { trashingEstate: false },
+      expected: { trashingEstate: false },
+    },
+  ])('when $cond, returns $expected', ({ t3, t4, expected }) => {
+    expect(resultOfTrashingEstate(t3, t4)).toEqual(expected)
+  })
+})
+
+describe('resultOfTrashingEstateAndAtLeastOnce', () => {
+  it.each([
+    {
+      cond: 'only trashing estate is satisfied',
+      t3: { trashingEstate: true, coin: 4 },
+      t4: { trashingEstate: false, coin: 4 },
+      expected: { trashingEstateAndAtLeastOnce5: false },
+    },
+    {
+      cond: 'only at least once coin is satisfied',
+      t3: { trashingEstate: false, coin: 4 },
+      t4: { trashingEstate: false, coin: 5 },
+      expected: { trashingEstateAndAtLeastOnce5: false },
+    },
+    {
+      cond: 'both are satisfied',
+      t3: { trashingEstate: true, coin: 4 },
+      t4: { trashingEstate: false, coin: 5 },
+      expected: { trashingEstateAndAtLeastOnce5: true },
+    },
+    {
+      cond: 'both are not satisfied',
+      t3: { trashingEstate: false, coin: 4 },
+      t4: { trashingEstate: false, coin: 4 },
+      expected: { trashingEstateAndAtLeastOnce5: false },
+    },
+  ])('when $cond returns $expected', ({ t3, t4, expected }) => {
+    expect(resultOfTrashingEstateAndAtLeastOnce(t3, t4, 5)).toEqual(expected)
+  })
+})
+
+describe('resultOfTrashingEstateAndAtLeastOnce5', () => {
+  it.each([
+    {
+      cond: 'only trashing estate is satisfied',
+      t3: { trashingEstate: true, coin: 4 },
+      t4: { trashingEstate: false, coin: 4 },
+      expected: { trashingEstateAndAtLeastOnce5: false },
+    },
+    {
+      cond: 'only at least once coin is satisfied',
+      t3: { trashingEstate: false, coin: 4 },
+      t4: { trashingEstate: false, coin: 5 },
+      expected: { trashingEstateAndAtLeastOnce5: false },
+    },
+    {
+      cond: 'both are satisfied',
+      t3: { trashingEstate: true, coin: 4 },
+      t4: { trashingEstate: false, coin: 5 },
+      expected: { trashingEstateAndAtLeastOnce5: true },
+    },
+    {
+      cond: 'both are not satisfied',
+      t3: { trashingEstate: false, coin: 4 },
+      t4: { trashingEstate: false, coin: 4 },
+      expected: { trashingEstateAndAtLeastOnce5: false },
+    },
+  ])('when $cond returns $expected', ({ t3, t4, expected }) => {
+    expect(resultOfTrashingEstateAndAtLeastOnce5(t3, t4)).toEqual(expected)
   })
 })
