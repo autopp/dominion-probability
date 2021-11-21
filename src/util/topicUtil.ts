d4bb8e0eac65ded5e6bf7c819838cc96e1e63ed8
autopp <autopp.inc@gmail.com>
2021-11-21 12:15:28 +0900

    feat: add topic type utility

--- src/util/topicUtil.ts
+++ src/util/topicUtil.ts
@@ -1,33 +1,36 @@
-export function topicForAtLeastOnce<Coin extends number>(
-  coin: Coin,
-  geq = true
-): { [t in `atLeastOnce${Coin}`]: string } {
+export type AtLeastOnce<N extends number> = `atLeastOnce${N}`
+export type Both<N extends number> = `both${N}`
+export type BothAndAtLeastOnce<B extends number, L extends number> = `both${B}AndAtLeastOnce${L}`
+export type TrashingEstate = 'trashingEstate'
+export type TrashingEstateAndAtLeastOnce<N extends number> = `trashingEstateAndAtLeastOnce${N}`
+
+export function topicForAtLeastOnce<Coin extends number>(coin: Coin, geq = true): { [t in AtLeastOnce<Coin>]: string } {
   return {
     [`atLeastOnce${coin}`]: `一度でも${coin}金${geq ? '以上' : ''}が出る確率`,
-  } as { [t in `atLeastOnce${Coin}`]: string }
+  } as { [t in AtLeastOnce<Coin>]: string }
 }
 
-export function topicForAtLeastOnce5(geq = true): { atLeastOnce5: string } {
+export function topicForAtLeastOnce5(geq = true): { [t in AtLeastOnce<5>]: string } {
   return topicForAtLeastOnce(5, geq)
 }
 
-export function topicForAtLeastOnce6(geq = true): { atLeastOnce6: string } {
+export function topicForAtLeastOnce6(geq = true): { [t in AtLeastOnce<6>]: string } {
   return topicForAtLeastOnce(6, geq)
 }
 
-export function topicForAtLeastOnce7(geq = true): { atLeastOnce7: string } {
+export function topicForAtLeastOnce7(geq = true): { [t in AtLeastOnce<7>]: string } {
   return topicForAtLeastOnce(7, geq)
 }
 
-export function topicForAtLeastOnce8(geq = true): { atLeastOnce8: string } {
+export function topicForAtLeastOnce8(geq = true): { [t in AtLeastOnce<8>]: string } {
   return topicForAtLeastOnce(8, geq)
 }
 
-export function topicForBoth<Coin extends number>(coin: Coin, geq = true): { [t in `both${Coin}`]: string } {
-  return { [`both${coin}`]: `両ターン共に${coin}金${geq ? '以上' : ''}が出る確率` } as { [t in `both${Coin}`]: string }
+export function topicForBoth<Coin extends number>(coin: Coin, geq = true): { [t in Both<Coin>]: string } {
+  return { [`both${coin}`]: `両ターン共に${coin}金${geq ? '以上' : ''}が出る確率` } as { [t in Both<Coin>]: string }
 }
 
-export function topicForBoth5(geq = true): { both5: string } {
+export function topicForBoth5(geq = true): { [t in Both<5>]: string } {
   return topicForBoth(5, geq)
 }
 
@@ -35,26 +38,26 @@ export function topicForBothAndAtLeastOnce<B extends number, L extends number>(
   bothCoin: B,
   atLeastOnceCoin: L,
   geq = true
-): { [t in `both${B}AndAtLeastOnce${L}`]: string } {
+): { [t in BothAndAtLeastOnce<B, L>]: string } {
   const key = `both${bothCoin}AndAtLeastOnce${atLeastOnceCoin}`
   return {
     [key]: `両ターン共に${bothCoin}金が出て、かつ一度でも${atLeastOnceCoin}金${geq ? '以上' : ''}が出る確率`,
-  } as { [t in `both${B}AndAtLeastOnce${L}`]: string }
+  } as { [t in BothAndAtLeastOnce<B, L>]: string }
 }
 
-export function topicForTrashingEstate(): { trashingEstate: string } {
+export function topicForTrashingEstate(): { [t in TrashingEstate]: string } {
   return { trashingEstate: '屋敷を廃棄できる確率' }
 }
 
 export function topicForTrashingEstateAndAtLeastOnce<Coin extends number>(
   coin: Coin,
   geq = true
-): { [t in `trashingEstateAndAtLeastOnce${Coin}`]: string } {
+): { [t in TrashingEstateAndAtLeastOnce<Coin>]: string } {
   return {
     [`trashingEstateAndAtLeastOnce${coin}`]: `屋敷を廃棄しつつ一度でも${coin}金${geq ? '以上' : ''}が出る確率`,
-  } as { [t in `trashingEstateAndAtLeastOnce${Coin}`]: string }
+  } as { [t in TrashingEstateAndAtLeastOnce<Coin>]: string }
 }
 
-export function topicForTrashingEstateAndAtLeastOnce5(geq = true): { trashingEstateAndAtLeastOnce5: string } {
+export function topicForTrashingEstateAndAtLeastOnce5(geq = true): { [t in TrashingEstateAndAtLeastOnce<5>]: string } {
   return topicForTrashingEstateAndAtLeastOnce(5, geq)
 }
