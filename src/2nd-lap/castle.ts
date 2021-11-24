4378bb1056e25743465a2fc616f38733645dacb6
autopp <autopp.inc@gmail.com>
2021-11-24 23:40:16 +0900

    feat: add castle

---
+++ src/2nd-lap/castle.ts
@@ -0,0 +1,80 @@
+import { run } from '@/runner'
+import { ACTION, Card, COPPER, ESTATE, Result, SILVER, Tactic } from '@/tactic'
+import {
+  AtLeastOnce,
+  Both,
+  genDecksWithSilverAndAction,
+  resultOfAtLeastOnces,
+  resultOfBoth5,
+  resultOfTrashingEstate,
+  resultOfTrashingEstateAndAtLeastOnce5,
+  simpleDeckPattern,
+  simulateTurnWithBaseCoinOnly,
+  splitByNoDraw,
+  sumOfCoin,
+  topicForAtLeastOnce5,
+  topicForAtLeastOnce6,
+  topicForBoth5,
+  topicForTrashingEstate,
+  topicForTrashingEstateAndAtLeastOnce5,
+  TrashingEstate,
+  TrashingEstateAndAtLeastOnce,
+  withCombinationOfEstates,
+} from '@/util'
+import { combination } from 'arubyray'
+
+type Topic = AtLeastOnce<5 | 6> | Both<5>
+
+abstract class Castle implements Tactic<[Card[], Card[]], Topic> {
+  abstract title(): string
+  abstract genDecks(): Card[][]
+
+  splitToHands = splitByNoDraw
+  patternsOfDeck = simpleDeckPattern
+
+  simulate(deck: [Card[], Card[]]): Result<Topic> {
+    const [t3, t4] = deck.map(simulateTurnWithBaseCoinOnly)
+
+    return {
+      ...resultOfAtLeastOnces(t3, t4, 5, 6),
+      ...resultOfBoth5(t3, t4),
+    }
+  }
+
+  topics(): { [t in Topic]: string } {
+    return {
+      ...topicForAtLeastOnce5(),
+      ...topicForAtLeastOnce6(false),
+      ...topicForBoth5(),
+    }
+  }
+}
+
+class CastleWithSilver extends Castle {
+  title = () => '銀貨・粗末な城納屋廃棄で4ターン目までに……'
+
+  genDecks() {
+    return withCombinationOfEstates(11, 2, (factory, otherIndices) =>
+      otherIndices.map((silver) =>
+        factory.create((deck) => {
+          deck[silver] = SILVER
+        })
+      )
+    )
+  }
+}
+
+class DoubleCastle extends Castle {
+  title = () => '粗末な城・粗末な城納屋廃棄で4ターン目までに……'
+
+  genDecks() {
+    return combination([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 2).map((others) => {
+      const deck = [COPPER, COPPER, COPPER, COPPER, COPPER, COPPER, COPPER, COPPER, COPPER, COPPER, COPPER]
+      deck[others[0]] = ESTATE
+      deck[others[1]] = ESTATE
+      return deck
+    })
+  }
+}
+
+run(new CastleWithSilver(), new DoubleCastle())
