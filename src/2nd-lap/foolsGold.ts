f7c1b658faf47bc20d892533a4bb06353a6e0444
autopp <autopp.inc@gmail.com>
2021-12-24 20:25:38 +0900

    feat: add foolsGold

---
+++ src/2nd-lap/foolsGold.ts
@@ -0,0 +1,56 @@
+import { run } from '@/runner'
+import { Card, Result, Tactic } from '@/tactic'
+import {
+  AtLeastOnce,
+  Both,
+  genDecksWithDouble,
+  resultOfAtLeastOnces,
+  resultOfBoth5,
+  simpleDeckPattern,
+  splitByNoDraw,
+  sumOfCoin,
+  topicForAtLeastOnce8,
+  topicForAtLeastOnces,
+  topicForBoth5,
+} from '@/util'
+
+type Topic = AtLeastOnce<5 | 6 | 7 | 8> | Both<5>
+
+const FOOLS_GOLD = 'foolsGold'
+
+class FoolsGold implements Tactic<[Card[], Card[]], Topic> {
+  readonly title = () => '愚者の黄金・愚者の黄金で4ターン目までに……'
+
+  genDecks = () => genDecksWithDouble(FOOLS_GOLD)
+  splitToHands = splitByNoDraw
+  patternsOfDeck = simpleDeckPattern
+
+  simulate(deck: [Card[], Card[]]): Result<Topic> {
+    const [t3, t4] = deck.map(this.simulateTurn)
+
+    return {
+      ...resultOfAtLeastOnces(t3, t4, 5, 6, 7, 8),
+      ...resultOfBoth5(t3, t4),
+    }
+  }
+
+  topics(): { [t in Topic]: string } {
+    return {
+      ...topicForAtLeastOnces(5, 6, 7),
+      ...topicForAtLeastOnce8(false),
+      ...topicForBoth5(),
+    }
+  }
+
+  private simulateTurn(hand: Card[]) {
+    let coin = sumOfCoin(hand)
+    const count = hand.filter((c) => c === FOOLS_GOLD).length
+    if (count > 0) {
+      coin += 4 * (count - 1) + 1
+    }
+
+    return { coin }
+  }
+}
+
+run(new FoolsGold())
