0ba92ce7cea75e7b3213618ad8e5444c2bdcb98a
autopp <autopp.inc@gmail.com>
2022-01-24 22:19:04 +0900

    feat: add doctor

---
+++ src/1st-lap/doctor.ts
@@ -0,0 +1,40 @@
+import { run } from '@/runner'
+import { Card, ESTATE, Result, Tactic } from '@/tactic'
+import { simpleDeckPattern, withCombinationOfEstates } from '@/util'
+import { take } from 'arubyray'
+
+type Topic = 'trashingOneEstate' | 'trashingTwoEstates'
+
+class Doctor implements Tactic<Card[], Topic> {
+  readonly title = () => '銅貨7枚・屋敷3枚で2ターン目までに……'
+
+  genDecks() {
+    return withCombinationOfEstates(5, (factory) => {
+      return [
+        factory.create(() => {
+          return
+        }),
+      ]
+    })
+  }
+  splitToHands = (deck: Card[]) => deck
+  patternsOfDeck = simpleDeckPattern
+
+  simulate(deck: Card[]): Result<Topic> {
+    const trashingEstate = take(deck, 2).filter((c) => c === ESTATE).length
+
+    return {
+      trashingOneEstate: trashingEstate === 1,
+      trashingTwoEstates: trashingEstate === 2,
+    }
+  }
+
+  topics(): { [t in Topic]: string } {
+    return {
+      trashingOneEstate: '屋敷を1枚廃棄できる確率',
+      trashingTwoEstates: '屋敷を2枚廃棄できる確率',
+    }
+  }
+}
+
+run(new Doctor())
