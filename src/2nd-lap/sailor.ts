488a5717ee224e579dbe78fc3ef12992da0ffb09
autopp <autopp.inc@gmail.com>
2022-05-19 21:45:37 +0900

    feat: add sailor

---
+++ src/2nd-lap/sailor.ts
@@ -0,0 +1,71 @@
+import { run } from '@/runner'
+import { ACTION, Card, ESTATE, Result, Tactic } from '@/tactic'
+import {
+  AtLeastOnce,
+  Both,
+  genDecksWithSilverAndAction,
+  resultOfAtLeastOnces,
+  resultOfBoth5,
+  resultOfTrashingEstate,
+  resultOfTrashingEstateAndAtLeastOnce5,
+  simpleDeckPattern,
+  splitByNoDraw,
+  sumOfCoin,
+  topicForAtLeastOnce5,
+  topicForAtLeastOnce6,
+  topicForBoth5,
+  topicForTrashingEstate,
+  topicForTrashingEstateAndAtLeastOnce5,
+  TrashingEstate,
+  TrashingEstateAndAtLeastOnce,
+} from '@/util'
+
+type Topic = AtLeastOnce<5 | 6> | Both<5> | TrashingEstate | TrashingEstateAndAtLeastOnce<5>
+
+class Sailor implements Tactic<[Card[], Card[]], Topic> {
+  readonly title = () => '銀貨・Sailor で4ターン目までに……'
+
+  genDecks = genDecksWithSilverAndAction
+  splitToHands = splitByNoDraw
+  patternsOfDeck = simpleDeckPattern
+
+  simulate(deck: [Card[], Card[]]): Result<Topic> {
+    const [hand3, hand4] = deck
+    const t3 = this.simulateTurn(hand3, false)
+    const t4 = this.simulateTurn(hand4, t3.duration)
+
+    return {
+      ...resultOfAtLeastOnces(t3, t4, 5, 6),
+      ...resultOfBoth5(t3, t4),
+      ...resultOfTrashingEstate(t3, t4),
+      ...resultOfTrashingEstateAndAtLeastOnce5(t3, t4),
+    }
+  }
+
+  topics(): { [t in Topic]: string } {
+    return {
+      ...topicForAtLeastOnce5(),
+      ...topicForAtLeastOnce6(false),
+      ...topicForBoth5(),
+      ...topicForTrashingEstate(),
+      ...topicForTrashingEstateAndAtLeastOnce5(),
+    }
+  }
+
+  private simulateTurn(hand: Card[], duration: boolean) {
+    let coin = sumOfCoin(hand)
+    let trashingEstate = false
+    if (duration) {
+      if (hand.includes(ESTATE)) {
+        coin += 2
+        trashingEstate = true
+      } else {
+        coin += 1
+      }
+    }
+
+    return { coin, duration: hand.includes(ACTION), trashingEstate }
+  }
+}
+
+run(new Sailor())
