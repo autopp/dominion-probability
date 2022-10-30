42e5e2bb00240822717803081395c0b026889835
autopp <autopp.inc@gmail.com>
2022-10-30 14:32:50 +0900

    feat: add two draw

--- src/2nd-lap/standard.ts
+++ src/2nd-lap/standard.ts
@@ -4,24 +4,26 @@ import {
   Both,
   BothAndAtLeastOnce,
   genDecksWithDoubleSilver,
+  genDecksWithSilverAndAction,
   resultOfBoth4,
   resultOfBothAndAtLeastOnce,
   simpleDeckPattern,
+  splitByDraw,
   splitByNoDraw,
   sumOfCoin,
   topicForBoth4,
   topicForBothAndAtLeastOnce,
 } from '@/util'
 
-type DoubleSilverTopic = Both<4> | BothAndAtLeastOnce<4, 5>
+type Topic = Both<4> | BothAndAtLeastOnce<4, 5>
 
-class DoubleSilver implements Tactic<[Card[], Card[]], DoubleSilverTopic> {
+class DoubleSilver implements Tactic<[Card[], Card[]], Topic> {
   title = () => '銀貨・銀貨で4ターン目までに……'
   genDecks = genDecksWithDoubleSilver
   splitToHands = splitByNoDraw
   patternsOfDeck = simpleDeckPattern
 
-  simulate(deck: [Card[], Card[]]): Result<DoubleSilverTopic> {
+  simulate(deck: [Card[], Card[]]): Result<Topic> {
     const [t3, t4] = deck.map(this.simulateTurn)
 
     return {
@@ -30,7 +32,7 @@ class DoubleSilver implements Tactic<[Card[], Card[]], DoubleSilverTopic> {
     }
   }
 
-  topics(): { [t in DoubleSilverTopic]: string } {
+  topics(): { [t in Topic]: string } {
     return {
       ...topicForBoth4(),
       ...topicForBothAndAtLeastOnce(4, 5),
@@ -42,4 +44,33 @@ class DoubleSilver implements Tactic<[Card[], Card[]], DoubleSilverTopic> {
   }
 }
 
-run(new DoubleSilver())
+class SilverWithTwoDraw implements Tactic<[Card[], Card[]], Topic> {
+  title = () => '銀貨・2ドローカード（堀など）で4ターン目までに……'
+  genDecks = genDecksWithSilverAndAction
+  splitToHands(deck: Card[]) {
+    return splitByDraw(deck, 2)
+  }
+  patternsOfDeck = simpleDeckPattern
+
+  simulate(deck: [Card[], Card[]]): Result<Topic> {
+    const [t3, t4] = deck.map(this.simulateTurn)
+
+    return {
+      ...resultOfBoth4(t3, t4),
+      ...resultOfBothAndAtLeastOnce(t3, t4, 4, 5),
+    }
+  }
+
+  topics(): { [t in Topic]: string } {
+    return {
+      ...topicForBoth4(),
+      ...topicForBothAndAtLeastOnce(4, 5),
+    }
+  }
+
+  private simulateTurn(hand: Card[]) {
+    return { coin: sumOfCoin(hand) }
+  }
+}
+
+run(new DoubleSilver(), new SilverWithTwoDraw())
